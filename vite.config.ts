18e0b88cfce7dfabbacc49b76f5672339fcff08e
Ryan Chen <11625854+rchen1991@users.noreply.github.com>
2026-04-18 17:28:28 -0400

    vercel deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/football-squares/',
+  base: process.env.VERCEL_BASE_PATH || '/football-squares/',
   plugins: [react()],
   server: {
     port: 3000,
